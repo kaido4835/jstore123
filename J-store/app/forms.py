61c9ff18808764450fcb08c7664ce2e8a7f3e650
Mironshoh <rahmatullaevmironshoh@gmail.com>
2025-01-12 16:14:58 +0500

    init commit

---
+++ J-store/app/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, FloatField, TextAreaField, IntegerField, SelectField, FileField
+from wtforms.validators import DataRequired, Email, EqualTo, NumberRange
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Confirm Password')
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class ProductForm(FlaskForm):
+    name = StringField('Product Name', validators=[DataRequired()])
+    description = TextAreaField('Description')
+    price = FloatField('Price', validators=[DataRequired(), NumberRange(min=0)])
+    category = StringField('Category')
+    color = StringField('Color')
+    material = StringField('Material')
+    stock_quantity = IntegerField('Stock Quantity', validators=[NumberRange(min=0)])
+    image = FileField('Product Image')
+    submit = SubmitField('Save')
+
